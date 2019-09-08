23c6cb7ed1e1728ee39c7c257b179b676b6fbbdc
HironoriSato <maza.hs5360@gmail.com>
2019-09-08 21:56:24 +0900

    [add]StringComp

---
+++ src/components/modules/StringComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{ string }}</p>
+</template>
+
+<script>
+export default {
+  name: "StringComp",
+  data() {
+    return {
+      string: "入力された感想を出力する"
+    };
+  }
+};
+</script>
