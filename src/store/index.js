ee93f9cd46bf12e9e62d7a809e209ab29c965c9c
HironoriSato <maza.hs5360@gmail.com>
2019-09-08 22:21:36 +0900

    [add]HeadCompでstoreからtitleの値を取得する処理を追加

--- src/store/index.js
+++ src/store/index.js
@@ -10,13 +10,36 @@ const Form = {
     mutations: {},
     actions: {
         buttonAction({ commit, state, rootState }) {
-            console.log("buttonAction")
+            commit('setStepCount', null, { root: true })  // rootへのアクセス
+        }
+    }
+}
+
+const Head = {
+    state: {
+        title: ['感想を入力', '確認画面', '送信完了']
+    },
+    mutations: {},
+    actions: {},
+    getters: {
+        getTitle(state, getters, rootState) {
+            return state.title[rootState.stepCount]  // rootStateのstepCountの値に応じてtitle配列の文字列を返す
         }
     }
 }
 
 export default new Vuex.Store({
+    state: {
+        stepCount: 0
+    },
+    mutations: {
+        setStepCount(state) {
+            console.log('rootsetStepCount')
+            state.stepCount++
+        }
+    },
     modules: {
-        Form
+        Form,
+        Head
     }
 })
